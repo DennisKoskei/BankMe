0d2d90172b2230f226f6edf0de540ab41249ede1
DennisKoskei <89400168+DennisKoskei@users.noreply.github.com>
2022-11-19 12:09:53 +0300

    Accessing the AccountActivity.java now successful

--- app/src/main/java/com/students/bankme/AccountActivity.java
+++ app/src/main/java/com/students/bankme/AccountActivity.java
@@ -10,38 +10,10 @@ import android.widget.EditText;
 
 public class AccountActivity extends AppCompatActivity {
 
-    CustomerActivity cust1 = new CustomerActivity(); // This is an object of the CustomerActivity Class.
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
 
-        EditText accountNameEditText = findViewById(R.id.accountNameEditText);
-        EditText accountNoEditText = findViewById(R.id.accountNoEditText);
-        EditText accountBalance = findViewById(R.id.accountBalanceEditText);
-
-        Button depositButton = findViewById(R.id.depositButton);
-        Button withdrawButton = findViewById(R.id.withdrawButton);
-        Button logoutButton = findViewById(R.id.logoutButton);
-        Button aboutButton = findViewById(R.id.aboutActionButton);
-
-        accountNameEditText.setText(cust1.fullName);
-        accountNoEditText.setText(cust1.accountNo);
-
-        depositButton.setOnClickListener(view -> {
-            // Code ...
-            // Learn how to start a small dialog where the user can input a certain amount to deposit or withdraw
-        });
-        withdrawButton.setOnClickListener(view -> {
-            // Code goes here ...
-            // Learn how to start a small dialog where the user can input a certain amount to deposit or withdraw
-        });
-        logoutButton.setOnClickListener(view -> {
-            finish();
-        });
-        aboutButton.setOnClickListener(view -> {
-            startActivity(new Intent(AccountActivity.this, AboutScrollingActivity.class));
-        });
     }
 }
